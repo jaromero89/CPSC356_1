02859a4da67802ec1dee0f32d0f86c878e80cbd0
jaromero89 <jaromero@chapman.edu>
2019-10-02 19:31:59 -0700

    Updated both interfaces- Now user is able to input username and press the login button -This allows application to save the numerical counter data.

--- app/src/main/java/com/linz/miky/cpsc356/MainActivity.kt
+++ app/src/main/java/com/linz/miky/cpsc356/MainActivity.kt
@@ -1,19 +1,26 @@
 package com.linz.miky.cpsc356
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.linz.miky.cpsc356.util.rotate90
 import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-  private var babyCounter: Int = 0
+  private var babyCounter: Long = 0
+  fun getStore() = getPreferences(Context.MODE_PRIVATE)
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
+    babyCounter = getStore().getLong(intent.extras?.get("username").toString(), babyCounter)
+    intent.extras?.get("username")
+
     if (savedInstanceState != null) {
-      babyCounter = savedInstanceState.getInt(BABY_COUNTER_KEY, 0)
+      updateCounter(savedInstanceState.getLong(intent.extras?.get("username").toString()))
+    }else if(getStore().contains(intent.extras?.get("username").toString())) {
       myTextView.text = babyCounter.toString()
     }
 
@@ -24,26 +31,31 @@ class MainActivity : AppCompatActivity() {
 
       myButton.text = when (babyCounter) {
         1L -> "stop"
-        in 2 .. 9 -> myButton.text.toString().plus(other:"!")
+        in 2 .. 9 -> myButton.text.toString().plus(  "!")
         else -> myButton.text
       }
     }
   }
 
+  private fun updateCounter(count: Long) {
+    babyCounter = count
+    myTextView.text =babyCounter.toString()
+  }
+
   override fun onPause() {
     super.onPause()
-    getStore().edit().putlong(BABY_COUNTER_KEY, babyCounter).apply()
+    getStore().edit().putLong(intent.extras?.get("username").toString(), babyCounter).apply()
   }
 
   override fun onSaveInstanceState(outState: Bundle?) {
     outState?.run {
-      putLong(BABY_COUNTER_KEY, babyCounter)
+      putLong( intent.extras?.get("username").toString(), babyCounter)
     }
 
     super.onSaveInstanceState(outState)
   }
 
   companion object {
-    private const val BABY_COUNTER_KEY = "babyCounterKey"
+    private const val BABY_COUNTER_KEY = ""
   }
 }
